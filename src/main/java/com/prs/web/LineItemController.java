564719292a60d38339d227c83c540177b860ace0
Emilie Nerl <emilie.nerl1@gmail.com>
2020-11-21 08:43:00 -0500

    updated recalculate total

--- src/main/java/com/prs/web/LineItemController.java
+++ src/main/java/com/prs/web/LineItemController.java
@@ -14,53 +14,87 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import com.prs.business.LineItem;
+import com.prs.business.Product;
+import com.prs.business.Request;
 import com.prs.db.LineItemRepo;
+import com.prs.db.RequestRepo;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/lineItems")
+@RequestMapping("/line-items")
 public class LineItemController {
 	
 	@Autowired
 	private LineItemRepo lineItemRepo;
-
+	
+	@Autowired
+	private RequestRepo requestRepo;
+	
+	// Get all LineItems
 	@GetMapping("/")
-	private List <LineItem> getAllLineItems() {
+	public List<LineItem> getAll() {
 		return lineItemRepo.findAll();
 	}
-
-	@GetMapping("{/id}")
-	private Optional <LineItem> getLineItemById(@PathVariable int id) {
+	
+	// Get a LineItem by id
+	@GetMapping("/{id}")
+	public Optional<LineItem> getById(@PathVariable int id) {
 		return lineItemRepo.findById(id);
 	}
-
+	
+	// Add a LineItem
 	@PostMapping("/")
-	private LineItem postLineItem(@RequestBody LineItem l) {
-		return lineItemRepo.save(l);
+	public LineItem addLineItem(@RequestBody LineItem litem) {
+		litem = lineItemRepo.save(litem);
+		recalculateTotal(litem);
+		return litem;
 	}
-
+	
+	// Update a LineItem
 	@PutMapping("/")
-	private LineItem putLineItem(@RequestBody LineItem l) {
-		return lineItemRepo.save(l);
+	public LineItem updateLineItem(@RequestBody LineItem litem) {
+		litem = lineItemRepo.save(litem);
+	//recalculate total	
+		recalculateTotal(litem);
+		return litem;
 	}
 	
+	// Delete a LineItem by id
+	@DeleteMapping("{id}")
+	public LineItem deleteLineItem(@PathVariable int id) {
+		Optional<LineItem> litem = lineItemRepo.findById(id);
 
-	@DeleteMapping("{/id}")
-	private LineItem deleteLineItem(@PathVariable int id) {
-		Optional <LineItem> l = lineItemRepo.findById(id);
-		if(l.isPresent()) {
+		if (litem.isPresent()) {
 			lineItemRepo.deleteById(id);
+			recalculateTotal(litem.get());
 		} else {
-		System.out.println("Error - cannot find line item" + id + "to delete");
+			System.out.println("Error - line item not found for id " + id);
 		}
-	return l.get();
+		return litem.get();
 	}
 	
-	// Get all LineItems by request ID
-		@GetMapping("/lines-for-pr/{id}")
-		public List<LineItem> getAllLineItemsByRequestId(@PathVariable int id) {
-			return lineItemRepo.findByRequestId(id);
-
-}
+	// Get all LineItems by Request ID
+	@GetMapping("/lines-for-pr/{id}")
+	public List<LineItem> getAllLineItemsByRequestId(@PathVariable int id) {
+		return lineItemRepo.findByRequestId(id);
+	}
+	
+	// Recalculates the Requests total based on CRUD functions LineItem Add, Update or Delete
+	public void recalculateTotal(LineItem litem) {	
+		List<LineItem> lineItems = lineItemRepo.findByRequestId(litem.getRequest().getId());
+		
+		//loop through the list of line items
+		double total = 0.0;
+		for(LineItem lineItem : lineItems) {
+			Product p = lineItem.getProduct();
+			total += (p.getPrice() * lineItem.getQuantity());
+		}
+		
+		Request request = litem.getRequest();
+		request.setTotal(total);
+		requestRepo.save(request);
+	}
+	
 }
